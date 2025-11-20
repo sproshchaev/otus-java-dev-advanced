d70bb5f1ea4c3e1c0a5260e0ac2d4650478c383e
sproshchaev <sproshchaev@gmail.com>
2025-11-20 16:14:17 +0500

    init

---
+++ src/main/java/ru/otus/Main.java
@@ -0,0 +1,12 @@
+package ru.otus;
+
+/**
+ * Java Developer. Advanced
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+
+}
